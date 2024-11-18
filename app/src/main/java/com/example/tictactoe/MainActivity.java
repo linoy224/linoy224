8012108f740ec7c0d8973f9f1f25b3810d55feb2
Linoy224 <linoys242@gmail.com>
2024-11-18 15:13:10 +0200

    fix crash

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        onNewGame();
     }
 
     private void onNewGame() {
